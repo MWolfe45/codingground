b8578d83c306c3ba45ee409e738aef730056b5b6
MWolfe45 <mwolfe45@gmail.com>
2017-05-18 10:27:37 -0500

    Created by Coding Ground Client

---
+++ New Project-20170518/main.py
@@ -0,0 +1,34 @@
+myList1 = []
+
+myList2 = [0]*10
+#print (myList2)
+
+##words = ['cat', 'window', 'defenestrate']
+##for w in words:
+##    print(w, len(w))
+##    print (w)
+    
+#for i in myList2:
+#    print (i)
+    
+for i in range(10):
+    myList2[i] = i
+    #print(i)    
+    
+#print (myList2)    
+    
+a = 1
+b = 1
+
+if b > a:
+    print ('b > a')
+elif b == a:
+    print ('b = a')
+else:
+    print ('b < a')
+
+if b == a:
+    print ('b = a')
+elif b !=a:
+    print ('b != a')
+    
